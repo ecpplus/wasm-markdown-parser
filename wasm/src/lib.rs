189604fbb0be15a809e540fec2db9787e33d89a0
Keita Arai <chu@ecp.plus>
2021-06-20 18:14:18 +0900

    added wasm

---
+++ wasm/src/lib.rs
@@ -0,0 +1,36 @@
+extern crate cfg_if;
+extern crate markdown;
+extern crate wasm_bindgen;
+
+mod utils;
+
+use cfg_if::cfg_if;
+use wasm_bindgen::prelude::*;
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn to_html(md_string: &str) -> String {
+    let html: String = markdown::to_html(&md_string);
+    log(&format!(
+        "markdown is\n {}\n\nhtml is\n{}",
+        &md_string, &html
+    ));
+    html
+}
